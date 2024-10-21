f16aa4bf160396c904268dd58dee95f19aa6af2e
upxin <upxin1126@163.com>
2024-10-21 23:43:12 +0800

    fix

---
+++ src/utils/menu.ts
@@ -0,0 +1,28 @@
+import type { MenuOption } from 'naive-ui';
+
+export const menuOptions: MenuOption[] = [
+  {
+    label: '首页',
+    key: 'home',
+  },
+  {
+    label: '无人机平台 + AI识别',
+    key: 'ai',
+  },
+  {
+    label: '三维地图（数字孪生 ）',
+    key: 'map3',
+  },
+  {
+    label: '数据建模（OSGB、3dmax）',
+    key: '3dmax',
+  },
+  {
+    label: '项目经验',
+    key: 'project',
+  },
+  {
+    label: '联系我们',
+    key: 'contact',
+  },
+];
---
+++ src/views/data/Card.vue
@@ -0,0 +1,13 @@
+<script setup lang="tsx">
+// JSX passed directly
+let msg = $ref(123);
+msg = 456;
+
+defineRender(
+  <div>
+    <span>
+      { msg }
+    </span>
+  </div>,
+);
+</script>
